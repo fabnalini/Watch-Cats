8b356d9452ea8d38ed035572f35ae6f9224eee8c
Bene <alvespbene@gmail.com>
2022-11-19 16:54:19 -0300

    correção de dependencias, texturas e scripts

--- root/src/ingame/cenario/casa/porta.gd
+++ root/src/ingame/cenario/casa/porta.gd
@@ -3,4 +3,4 @@ extends Area2D
 # muda sala :>
 func _on_porta_body_entered(_body):
 	Global.lugar = get_parent().name
-	get_tree().change_scene("res://src/rooms/casa/"+ self.name +".tscn")
+	get_tree().change_scene("res://src/ingame/cenario/casa/"+ self.name +".tscn")
