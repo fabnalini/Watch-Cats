e79d09f72279e46b9438425de7eaf25cb13aaab3
Bene <alvespbene@gmail.com>
2022-11-20 14:58:12 -0300

    polimento

--- root/src/ingame/stage/minigame1/enemy-ship.gd
+++ root/src/ingame/stage/minigame1/enemy-ship.gd
@@ -4,7 +4,7 @@ func _physics_process(_delta: float) -> void:
 	var inimigos_restantes = get_tree().get_nodes_in_group("enemies").size()
 	
 	if (inimigos_restantes == 1):
-		get_tree().change_scene("res://src/ingame/cenario/casa/quarto.tscn")
+		get_tree().change_scene("res://src/interface/fim_prototipo.tscn")
 
 func _on_enemyship_body_entered(body):
 	queue_free()
