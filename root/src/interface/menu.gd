7a0a6efde23a7910cd58cba43b27b6b004566482
GafCastillo <saulgabriel.ct@gmail.com>
2022-11-20 19:32:30 -0300

    Agora vai Confia

--- root/src/interface/menu.gd
+++ root/src/interface/menu.gd
@@ -1,7 +1,15 @@
 extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/btn_start.grab_focus()
 
 func _on_btn_start_pressed():
 	get_tree().change_scene("res://src/ingame/cenario/casa/quarto.tscn")
+
+
+func _on_btn_load_pressed():
+	get_tree().change_scene("res://src/interface/fim_prototipo.tscn")
+
+
+func _on_btn_options_pressed():
+	get_tree().change_scene("res://src/interface/fim_prototipo.tscn")
--- root/src/interface/prototipo.gd
+++ root/src/interface/prototipo.gd
@@ -2,3 +2,7 @@ extends Sprite
 
 func _physics_process(_delta: float) -> void:
 	$AnimationPlayer.play("animation")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://src/interface/menu.tscn")
